6bcff1d197c620a3cbaf9216035efdeb11f7df4f
lorendsnow <lorendsnow@gmail.com>
2024-04-03 21:15:17 -0700

    Delete due to event patter refactoring

--- IBKRWrapper/Models/OrderKey.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IBKRWrapper.Models
-{
-    internal struct OrderKey
-    {
-        public int? ClientId { get; set; }
-        public int? OrderId { get; set; }
-        public int? PermId { get; set; }
-    }
-}
