9dd0480198eacb7f466860cb25435572453ea8cc
lorendsnow <lorendsnow@gmail.com>
2024-04-05 14:55:49 -0700

    Change from class to record

--- IBKRWrapper/Models/OrderStatus.cs
+++ IBKRWrapper/Models/OrderStatus.cs
@@ -1,27 +1,17 @@
 ﻿namespace IBKRWrapper.Models
 {
-    public class OrderStatus(
-        int orderId,
-        string status,
-        decimal filled,
-        decimal remaining,
-        double avgFillPrice,
-        int permId,
-        int parentId,
-        double lastFillPrice,
-        string whyHeld
+    public record OrderStatus(
+        int OrderId,
+        string Status,
+        decimal Filled,
+        decimal Remaining,
+        double AvgFillPrice,
+        int PermId,
+        int ParentId,
+        double LastFillPrice,
+        string WhyHeld
     )
     {
-        public int OrderId { get; set; } = orderId;
-        public string Status { get; set; } = status;
-        public decimal Filled { get; set; } = filled;
-        public decimal Remaining { get; set; } = remaining;
-        public double AvgFillPrice { get; set; } = avgFillPrice;
-        public int PermId { get; set; } = permId;
-        public int ParentId { get; set; } = parentId;
-        public double LastFillPrice { get; set; } = lastFillPrice;
-        public string WhyHeld { get; set; } = whyHeld;
-
         public bool Accepted
         {
             get
