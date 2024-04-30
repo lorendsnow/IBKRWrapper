ed69b453ee50c4ee77efe7a73637053bc37cb021
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-04-30 09:37:18 -0700

    initial commit

---
+++ IBKRWrapper/Constants/OptionTickIds.cs
@@ -0,0 +1,21 @@
+namespace IBKRWrapper.Constants
+{
+    public enum OptionTickIds
+    {
+        BidSize = 0,
+        BidPrice = 1,
+        AskPrice = 2,
+        AskSize = 3,
+        LastPrice = 4,
+        LastSize = 5,
+        High = 6,
+        Low = 7,
+        Volume = 8,
+        BidOptionComputation = 10,
+        AskOptionComputation = 11,
+        LastOptionComputation = 12,
+        ModelOptionComputation = 13,
+        OpenTick = 14,
+        LastTimestamp = 45,
+    }
+}
