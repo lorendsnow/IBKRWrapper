a9f3ef11b42db851ce9d620a6df1342f0431286b
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-04-30 14:11:12 -0700

    use livemarketdata base class

--- IBKRWrapper/Models/OptionMarketData.cs
+++ IBKRWrapper/Models/OptionMarketData.cs
@@ -5,27 +5,14 @@ using IBApi;
 
 namespace IBKRWrapper.Models
 {
-    public class OptionMarketData(int reqId, Contract contract)
+    public class OptionMarketData(int reqId, Contract contract) : LiveMarketData(reqId, contract)
     {
-        public int ReqId { get; init; } = reqId;
-        public Contract Contract { get; init; } = contract;
-        public List<double> BidPrices { get; private set; } = [];
-        public List<decimal> BidSizes { get; private set; } = [];
-        public List<double> AskPrices { get; private set; } = [];
-        public List<decimal> AskSizes { get; private set; } = [];
-        public List<double> LastPrices { get; private set; } = [];
-        public List<decimal> LastSizes { get; private set; } = [];
-        public List<double> Highs { get; private set; } = [];
-        public List<double> Lows { get; private set; } = [];
-        public List<decimal> Volumes { get; private set; } = [];
         public List<OptionGreeks> BidGreeks { get; private set; } = [];
         public List<OptionGreeks> AskGreeks { get; private set; } = [];
         public List<OptionGreeks> LastGreeks { get; private set; } = [];
         public List<OptionGreeks> ModelGreeks { get; private set; } = [];
-        public double? OpenTick { get; private set; }
-        public List<DateTimeOffset> TimeStamps { get; private set; } = [];
 
-        public void UpdateMarketData<T>(object? sender, MarketDataEventArgs<T> e)
+        public override void UpdateMarketData<T>(object? sender, MarketDataEventArgs<T> e)
         {
             if (e.Data.RequestId != ReqId) return;
 
