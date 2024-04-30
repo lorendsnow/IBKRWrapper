c9e92f53b98d8e9b315f2a8925e0f6c0b94586df
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-04-30 14:08:42 -0700

    initial commit

---
+++ IBKRWrapper/Models/LiveMarketData.cs
@@ -0,0 +1,27 @@
+namespace IBKRWrapper.Models
+{
+    public abstract class LiveMarketData
+    {
+        public required int ReqId { get; init; } 
+        public required Contract Contract { get; init; }
+        public List<double> BidPrices { get; private set; } = [];
+        public List<decimal> BidSizes { get; private set; } = [];
+        public List<double> AskPrices { get; private set; } = [];
+        public List<decimal> AskSizes { get; private set; } = [];
+        public List<double> LastPrices { get; private set; } = [];
+        public List<decimal> LastSizes { get; private set; } = [];
+        public List<double> Highs { get; private set; } = [];
+        public List<double> Lows { get; private set; } = [];
+        public List<decimal> Volumes { get; private set; } = [];
+        public double? OpenTick { get; private set; }
+        public List<DateTimeOffset> TimeStamps { get; private set; } = [];
+
+        protected LiveMarketData(int reqId, Contract contract)
+        {
+            ReqId = reqId;
+            Contract = contract;
+        }
+
+        public abstract void UpdateMarketData<T>(object? sender, MarketDataEventArgs<T> e);
+    }
+}
