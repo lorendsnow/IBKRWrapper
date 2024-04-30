7aa9c0c086ee92bf99016728a25bd0fdb422d9aa
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-04-30 09:31:27 -0700

    initial commit

---
+++ IBKRWrapper/Constants/EquityTickIds.cs
@@ -0,0 +1,17 @@
+namespace IBKRWrapper.Constants
+{
+    public enum EquityTickIds
+    {
+        BidSize = 0,
+        BidPrice = 1,
+        AskPrice = 2,
+        AskSize = 3,
+        LastPrice = 4,
+        LastSize = 5,
+        High = 6,
+        Low = 7,
+        Volume = 8,
+        OpenTick = 14,
+        LastTimestamp = 45,
+    }
+}
