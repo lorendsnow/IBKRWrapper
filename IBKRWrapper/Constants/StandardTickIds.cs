9e5a3e21edd2c0dd48947676c95ee31679fd4a31
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-05-01 08:34:35 -0700

    consolidate standard ticks into one enum

--- IBKRWrapper/Constants/StandardTickIds.cs
+++ IBKRWrapper/Constants/StandardTickIds.cs
@@ -1,6 +1,6 @@
 namespace IBKRWrapper.Constants
 {
-    public enum OptionTickIds
+    public enum StandardTickIds
     {
         BidSize = 0,
         BidPrice = 1,
@@ -11,11 +11,16 @@ namespace IBKRWrapper.Constants
         High = 6,
         Low = 7,
         Volume = 8,
+        ClosePrice = 9,
         BidOptionComputation = 10,
         AskOptionComputation = 11,
         LastOptionComputation = 12,
         ModelOptionComputation = 13,
         OpenTick = 14,
+        BidExchange = 32,
+        AskExchange = 33,
         LastTimestamp = 45,
+        Halted = 49,
+        LastExchange = 84,
     }
 }
