da902d688fa2f6c580c564b6758a7f2b0824aa1c
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-05-01 09:06:00 -0700

    consolidate all marketdata results into this object

--- IBKRWrapper/Models/LiveMarketData.cs
+++ IBKRWrapper/Models/LiveMarketData.cs
@@ -3,10 +3,10 @@ using IBKRWrapper.Events;
 
 namespace IBKRWrapper.Models
 {
-    public abstract class LiveMarketData
+    public class LiveMarketData(int reqId, Contract, contract)
     {
-        public int ReqId { get; init; }
-        public Contract Contract { get; init; }
+        public int ReqId { get; init; } = reqId;
+        public Contract Contract { get; init; } = contract;
         public List<double> BidPrices { get; private set; } = [];
         public List<decimal> BidSizes { get; private set; } = [];
         public List<double> AskPrices { get; private set; } = [];
@@ -16,26 +16,124 @@ namespace IBKRWrapper.Models
         public List<double> Highs { get; private set; } = [];
         public List<double> Lows { get; private set; } = [];
         public List<decimal> Volumes { get; private set; } = [];
-        public double? OpenTick { get; protected set; }
+        public double? CloseTick ( get; private set; }
+        public double? OpenTick { get; private set; }
         public List<DateTimeOffset> TimeStamps { get; private set; } = [];
+        public List<OptionGreeks> BidGreeks { get; private set; } = [];
+        public List<OptionGreeks> AskGreeks { get; private set; } = [];
+        public List<OptionGreeks> LastGreeks { get; private set; } = [];
+        public List<OptionGreeks> ModelGreeks { get; private set; } = [];
+        public bool Halted { get; private set; }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage(
-            "Style",
-            "IDE0290:Use primary constructor",
-            Justification = "Shouldn't use a public constructor in an abstract class"
-        )]
-        protected LiveMarketData(int reqId, Contract contract)
+        public void UpdateMarketData(object? sender, MarketDataEventArgs<double> e)
         {
-            ReqId = reqId;
-            Contract = contract;
+            if (e.Data.ReqId != ReqId)
+                return;
+
+            switch ((StandardTickIds)e.Data.Field)
+            {
+                case StandardTickIds.BidPrice:
+                    BidPrices.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.AskPrice:
+                    AskPrices.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.LastPrice:
+                    LastPrices.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.High:
+                    Highs.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.Low:
+                    Lows.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.ClosePrice:
+                    CloseTick ??= e.Data.Value;
+                    break;
+
+                case StandardTickIds.OpenTick:
+                    OpenTick ??= e.Data.Value;
+                    break;
+
+                case StandardTickIds.Halted:
+                    Halted = e.Data.Value == (1 || 2) ? true : false;
+                    break;
+
+                default:
+                    break;
+            }
         }
 
-        public abstract void UpdateMarketData(object? sender, MarketDataEventArgs<double> e);
+        public void UpdateMarketData(object? sender, MarketDataEventArgs<decimal> e)
+        {
+            if (e.Data.ReqId != ReqId)
+                return;
 
-        public abstract void UpdateMarketData(object? sender, MarketDataEventArgs<decimal> e);
+            switch ((StandardTickIds)e.Data.Field)
+            {
+                case StandardTickIds.BidSize:
+                    BidSizes.Add(e.Data.Value);
+                    break;
 
-        public abstract void UpdateMarketData(object? sender, MarketDataEventArgs<string> e);
+                case StandardTickIds.AskSize:
+                    AskSizes.Add(e.Data.Value);
+                    break;
 
-        public abstract void UpdateMarketData(object? sender, MarketDataEventArgs<OptionGreeks> e);
+                case StandardTickIds.LastSize:
+                    LastSizes.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.Volume:
+                    Volumes.Add(e.Data.Value);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void UpdateMarketData(object? sender, MarketDataEventArgs<OptionGreeks> e)
+        {
+            if (e.Data.ReqId != ReqId)
+                return;
+            
+            switch ((StandardTickIds)e.Data.Field)
+            {
+                case StandardTickIds.BidOptionComputation:
+                    BidGreeks.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.AskOptionComputation:
+                    AskGreeks.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.LastOptionComputation:
+                    LastGreeks.Add(e.Data.Value);
+                    break;
+
+                case StandardTickIds.ModelOptionComputation:
+                    ModelGreeks.Add(e.Data.Value);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void UpdateMarketData(object? sender, MarketDataEventArgs<string> e)
+        {
+            if (e.Data.ReqId != ReqId)
+                return;
+
+            if ((StandardTickIds)e.Data.Field == StandardTickIds.LastTimestamp)
+            {
+                TimeStamps.Add(IbDateParser.ParseIBDateTime(e.Data.Value));
+            }
+        }
     }
 }
