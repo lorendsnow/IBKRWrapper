c469791defec5cd29b8e21de3167ed500070c678
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-05-02 14:38:08 -0700

    initial commit

---
+++ IBKRWrapper/Constants/BarDataType.cs
@@ -0,0 +1,20 @@
+namespace IBKRWrapper.Constants
+{
+    public enum BarDataType
+    {
+    	AGGTRADES,
+    	ASK,
+    	BID,
+    	BID_ASK,
+    	FEERATE,
+    	HISTORICAL_VOLATILITY,
+    	MIDPOINT,
+    	OPTION_IMPLIED_VOLATILITY,
+    	SCHEDULE,
+    	TRADES,
+    	YIELD_ASK,
+    	YIELD_BID,
+    	YIELD_BID_ASK,
+    	YIELD_LAST,
+    }
+}
