122e783b6a3b22c0ed8a3ea00170e8b8e251d6d7
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-07-08 13:29:59 -0700

    initial commit

---
+++ SimpleBroker/Data/HistoricalTick.cs
@@ -0,0 +1,28 @@
+﻿namespace SimpleBroker
+{
+    /// <summary>
+    /// Represents a historical midprice tick
+    /// </summary>
+    public record HistoricalTick
+    {
+        /// <summary>
+        /// The underlying instrument's symbol
+        /// </summary>
+        public required string Symbol { get; init; }
+
+        /// <summary>
+        /// The tick's timestamp
+        /// </summary>
+        public DateTimeOffset Time { get; init; }
+
+        /// <summary>
+        /// The midprice
+        /// </summary>
+        public double Price { get; init; }
+
+        /// <summary>
+        /// The size of the tick
+        /// </summary>
+        public decimal Size { get; init; }
+    }
+}
