3a3b26c007413d6306141112c61467a9662827ca
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-07-08 13:18:18 -0700

    initial commit

---
+++ SimpleBroker/Data/HistoricalTickBidAsk.cs
@@ -0,0 +1,38 @@
+﻿namespace SimpleBroker
+{
+    /// <summary>
+    /// Represents a historical bid/ask tick
+    /// </summary>
+    public record HistoricalTickBidAsk
+    {
+        /// <summary>
+        /// The underlying instrument's symbol
+        /// </summary>
+        public required string Symbol { get; init; }
+
+        /// <summary>
+        /// The tick's timestamp
+        /// </summary>
+        public DateTimeOffset Time { get; init; }
+
+        /// <summary>
+        /// The bid price
+        /// </summary>
+        public double PriceBid { get; init; }
+
+        /// <summary>
+        /// The ask price
+        /// </summary>
+        public double PriceAsk { get; init; }
+
+        /// <summary>
+        /// The bid size
+        /// </summary>
+        public decimal SizeBid { get; init; }
+
+        /// <summary>
+        /// The ask size
+        /// </summary>
+        public decimal SizeAsk { get; init; }
+    }
+}
