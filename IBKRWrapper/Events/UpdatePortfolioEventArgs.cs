469007eb0df0e573ed3c0f76cbd232c2f1d2703c
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-07-09 13:37:48 -0700

    change to use raw event args

--- IBKRWrapper/Events/UpdatePortfolioEventArgs.cs
+++ IBKRWrapper/Events/UpdatePortfolioEventArgs.cs
@@ -1,9 +1,25 @@
-﻿using IBKRWrapper.Models;
+﻿using IBApi;
 
 namespace IBKRWrapper.Events
 {
-    public class UpdatePortfolioEventArgs(PortfolioPosition position) : EventArgs
+    public class UpdatePortfolioEventArgs(
+        Contract contract,
+        decimal position,
+        double marketPrice,
+        double marketValue,
+        double averageCost,
+        double unrealizedPNL,
+        double realizedPNL,
+        string accountName
+    ) : EventArgs
     {
-        public PortfolioPosition Position { get; private set; } = position;
+        public Contract Contract { get; } = contract;
+        public decimal Position { get; } = position;
+        public double MarketPrice { get; } = marketPrice;
+        public double MarketValue { get; } = marketValue;
+        public double AverageCost { get; } = averageCost;
+        public double UnrealizedPNL { get; } = unrealizedPNL;
+        public double RealizedPNL { get; } = realizedPNL;
+        public string Account { get; } = accountName;
     }
 }
