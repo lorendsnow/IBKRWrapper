ce91603a37ac24a0bf1f827e3d1454663eea10ef
lorendsnow <141444165+lorendsnow@users.noreply.github.com>
2024-07-09 16:58:21 -0700

    change to emit raw values

--- IBKRWrapper/Events/RealTimeBarEventArgs.cs
+++ IBKRWrapper/Events/RealTimeBarEventArgs.cs
@@ -1,10 +1,25 @@
-﻿using IBKRWrapper.Models;
-
-namespace IBKRWrapper.Events
+﻿namespace IBKRWrapper.Events
 {
-    public class RealTimeBarEventArgs(int reqId, RealTimeBar bar) : EventArgs
+    public class RealTimeBarEventArgs(
+        int reqId,
+        long time,
+        double open,
+        double high,
+        double low,
+        double close,
+        decimal volume,
+        decimal WAP,
+        int count
+    ) : EventArgs
     {
-        public int ReqId { get; private set; } = reqId;
-        public RealTimeBar RealTimeBar { get; private set; } = bar;
+        public int ReqId { get; } = reqId;
+        public DateTimeOffset Time { get; } = DateTimeOffset.FromUnixTimeSeconds(time);
+        public double Open { get; } = open;
+        public double High { get; } = high;
+        public double Low { get; } = low;
+        public double Close { get; } = close;
+        public decimal Volume { get; } = volume;
+        public decimal WAP { get; } = WAP;
+        public int Count { get; } = count;
     }
 }
